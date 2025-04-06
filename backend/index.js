1663bd8c795393426d0d51300a00c8f0d38a019e
Shubham Singh <shubhamofficial23kl@gmail.com>
2025-04-06 14:39:21 +0530

    making routed in proper way

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express=require("express")
 const app=express()
+const router=require("./routes/index")
 const port=3000
+const cors=require("cors")
+
+app.use(cors())
+app.use(express.json())
+app.use("api/v1",router)
 
 app.listen(port,function(){
     console.log("app is listening on the port"+port)
---
+++ backend/routes/index.js
@@ -0,0 +1,9 @@
+const express=require("express")
+const adminRouter=require("./adminRouter")
+const patientRouter=require("./patientRouter")
+
+const router =express.Router()
+router.use("/patient",patientRouter)
+router.use("/admin",adminRouter)
+
+module.export=router
